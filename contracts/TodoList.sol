e94d8ba6037ffb8a460a8ff274816ab89672bba3
Sebastian <sesantander@uninorte.edu.co>
2021-10-16 21:13:25 -0500

    Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList {
+  // el keyword public lo que hace es que nos deja leer taskCount como si fuera el llamado de una funcion
+  uint public taskCount = 0;
+}
