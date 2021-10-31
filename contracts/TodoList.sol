626ce57acd03ec03fa348fd6f969bf76137e0814
Sebastian <sesantander@uninorte.edu.co>
2021-10-30 20:45:35 -0500

    release 1.0.0: Version estable

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.4.22 <0.9.0;
 contract TodoList {
     // el keyword public lo que hace es que nos deja leer taskCount como si fuera el llamado de una funcion
     uint256 public taskCount = 0;
-    // address payable owner = msg.sender;
 
     struct Task {
         uint256 id;
@@ -21,7 +20,7 @@ contract TodoList {
     event TaskCreated(uint256 id, string content, bool completed, uint reward);
 
     constructor() public {
-      createTask("First task",1);
+      createTask("Do cryptography project", 1);
     }
 
     // funcion para guardar tasks dentro del mapping
@@ -31,30 +30,30 @@ contract TodoList {
       emit TaskCreated(taskCount, _content, false, _reward);
     }
 
-    function toggleCompleted(uint256 _id) public {
+    // funcion para para marcar como completa una tarea
+    function markCompleted(uint256 _id) public {
       Task memory _task = tasks[_id];
       _task.completed = !_task.completed;
       tasks[_id] = _task;
       emit TaskCompleted(_id, _task.completed);
     }
 
+    // funcion para enviar ether del contrato hacia una direccion
     function sendEther(address payable recipient, uint reward) external {
       uint money = reward * 1 ether;
       recipient.transfer(money);
     }
-
+    
+    // funcion para obtener el valor de la recompensa de cada task 
     function getTaskReward(uint256 _id) external view returns(uint){
       Task memory _task = tasks[_id];
       return _task.reward;
     }
 
-    //function recieveEther() payable public{}
-    // function recieveEther() external payable {
-    //   functionCalled = 'recieveEther';
-    // }
-
+    // funcion para depositar ether en el smart contract
     function() external payable {}
 
+    // funcion para mostrar cuanto ether tiene este smart contract
     function showBalance() external view returns (uint256) {
       return address(this).balance;
     }
--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ App = {
     await App.render()
   },
 
-  // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   loadWeb3: async () => {
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider
@@ -17,105 +16,89 @@ App = {
     } else {
       window.alert("Please connect to Metamask.")
     }
-    // Modern dapp browsers...
     if (window.ethereum) {
       window.web3 = new Web3(ethereum)
       try {
-        // Request account access if needed
         await ethereum.enable()
-        // Acccounts now exposed
         web3.eth.sendTransaction({/* ... */})
       } catch (error) {
-        // User denied account access...
       }
     }
-    // Legacy dapp browsers...
     else if (window.web3) {
       App.web3Provider = web3.currentProvider
       window.web3 = new Web3(web3.currentProvider)
-      // Acccounts always exposed
       web3.eth.sendTransaction({/* ... */})
     }
-    // Non-dapp browsers...
     else {
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   },
 
   loadAccount: async () => {
-    // Set the current blockchain account
+    // Cargamos la direccion conectada en metamask
     App.account = web3.eth.accounts[0]
   },
   loadContract: async () => {
-    // Create a JavaScript version of the smart contract
+    // Obtenemos una el smart contract 
     const todoList = await $.getJSON('TodoList.json')
     App.contracts.TodoList = TruffleContract(todoList)
    
-    console.log(web3.eth.networks)
     web3.eth.defaultAccount = web3.eth.accounts[0];
     App.contracts.TodoList.setProvider(App.web3Provider)
-    // Hydrate the smart contract with values from the blockchain
     App.todoList = await App.contracts.TodoList.deployed()
   },
   
   render: async () => {
-    // Prevent double render
     if (App.loading) {
       return
     }
-
-    // Update app loading state
     App.setLoading(true)
-    // Render Account
+    // Renderizamos la cuenta
     $('#account').html(App.account)
-    // Render Smart Contract Balance
+    // Renderizamos el balance del smart contract
     let balance = await App.todoList.showBalance()
     $('#balance').html( `Smart Contract Balance : ${balance?.c[0]/10000}.00 ETH`)
-    // Render Tasks
+    // Renderiuzamos las tareas
     await App.renderTasks()
 
-    // Update loading state
     App.setLoading(false)
   },
 
   renderTasks: async () => {
-    // Load the total task count from the blockchain
+    // Llamamos la variable taskCount del smart contract que va a tener el total de tareas
     const taskCount = await App.todoList.taskCount()
     const $taskTemplate = $('.taskTemplate')
 
-    // Render out each task with a new task template
     for (var i = 1; i <= taskCount; i++) {
-      // Fetch the task data from the blockchain
+      // Obtenemos el array de tareas del smart contract
       const task = await App.todoList.tasks(i)
       const taskId = task[0].toNumber()
       const taskContent = task[1]
       const taskCompleted = task[2]
       const taskReward = task[3]
 
-      // Create the html for the task
+      // Llenamos el html con la informacion de cada tarea
       const $newTaskTemplate = $taskTemplate.clone()
-      $newTaskTemplate.find('.content').html(taskContent + ' - ' + taskReward)
+      $newTaskTemplate.find('.content').html(taskContent + ' - ' + taskReward + " ETH")
       $newTaskTemplate.find('input')
                       .prop('name', taskId)
                       .prop('checked', taskCompleted)
                       .on('click', App.marked)
 
-      // Put the task in the correct list
       if (taskCompleted) {
         $('#completedTaskList').append($newTaskTemplate)
       } else {
         $('#taskList').append($newTaskTemplate)
       }
 
-      // Show the task
       $newTaskTemplate.show()
     }
   },
   depositEth: async () => {
-    
+    // Metodo para enviar ether de una cuenta a otra, en este caso enviamos ether a la direccion de el smart contract
     await web3.eth.sendTransaction({
-      from: '0xc8Df9C0bF5A503FB1E0eE982f52403CDeA30f1E5',
-      to: '0xe7ca6742E9584fCF4C779A0e3Bb0ACe21116f5a7', 
+      from: App.account,
+      to: '0x0cda18B7C9952ef4DF502c8D59435B1c3226438F', 
       value: web3.toWei(2, "ether"), 
     }, async function(err, transactionHash) {
         if (err) { 
@@ -130,6 +113,7 @@ App = {
     App.setLoading(true)
     const content = $('#newTask').val()
     const rewardValue = $('#reward').val()
+    // Una vez obtenidos los valores ingresados por el usuario creamos la tarea con el metodo de el smart contract
     await App.todoList.createTask(content,rewardValue)
     window.location.reload()
   },
@@ -137,11 +121,12 @@ App = {
   marked: async (e) => {
     App.setLoading(true)
     const taskId = e.target.name
+    // Obtenemos el valor del reward de la tarea con taskId
     let rewardValue = await App.todoList.getTaskReward(taskId)
-
-    await App.todoList.toggleCompleted(taskId)
-    
-    await App.todoList.sendEther('0xc8Df9C0bF5A503FB1E0eE982f52403CDeA30f1E5',rewardValue)
+    // Marcamos la tarea como completa
+    await App.todoList.markCompleted(taskId)
+    // Enviamos ether con el valor de la recompensa a la cuenta conectada
+    await App.todoList.sendEther(App.account, rewardValue)
     
     window.location.reload()
   },
